8bfa4a08272a087b69302ddb4d570ab620575e18
Vitor Barbosa <vitorricardosilvabarbosa@gmail.com>
2023-11-16 15:25:30 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var animeVitor = 'https://criticalhits.com.br/wp-content/uploads/2021/05/Tony-Tony-Chopper-One-Piece.jpg'
+var filmeVitor = 'https://cloud.estacaonerd.com/wp-content/uploads/2023/09/05041009/Park-Chan-wook_oldboy_1920x1080.jpg'
+var serieVitor = 'https://www.slsc.org/wp-content/uploads/2023/02/tardis-doctor-who.jpg'
+var cantorVitor = 'https://www.diariodocentrodomundo.com.br/wp-content/uploads/2015/01/TimMaiaPhoto.jpg'
+
+document.write(`<img src=${filmeVitor}>`)
+document.write(`<img src=${serieVitor}>`)
+document.write(`<img src=${animeVitor}>`)
+document.write(`<img src=${cantorVitor}>`)
